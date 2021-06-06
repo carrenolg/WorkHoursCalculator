54db4529a1cfe7788b52a893f4b6d50233d9bed6
Carrenolg <carrenolg@gmail.com>
2021-06-05 23:37:02 -0500

    api code review

--- services/web/app/app.py
+++ services/web/app/app.py
@@ -32,5 +32,5 @@ initialize_routes(api)
 
 # Custom errors doesn't work when debug=True is setting
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run(host='0.0.0.0', port=8080, debug=False)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
